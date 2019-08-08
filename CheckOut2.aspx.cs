0d69a4b1bfc4010355dd7c2f029a716ccce7d626
lekhanhhung9979 <50612412+lekhanhhung9979@users.noreply.github.com>
2019-08-08 19:12:30 +0700

    commit

---
+++ CheckOut2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CheckOut2 : System.Web.UI.Page
+{
+    private Customer customer;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        customer = (Customer)Session["Customer"];
+
+        String message = "Contact information" + "\n" +
+            "    Email: " + customer.EmailAddress + "\n" +
+            "    First name: " + customer.FirstName + "\n" +
+            "    Last name: " + customer.LastName + "\n" +
+            "    Phone number: " + customer.Phone + "\n" +
+            "Billing address" + "\n" +
+            "    Address: " + customer.Address + "\n" +
+            "    City: " + customer.City + "\n" +
+            "    State: " + customer.State + "\n" +
+            "    Zip code: " + customer.Zip + "\n";
+
+        txtData.Text = message;
+    }
+}
